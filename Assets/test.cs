6b8169662eacd259834e095de284bd9da77f99db
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2024-12-10 16:45:11 +0100

    add move 2 player

--- Assets/test.cs
+++ Assets/test.cs
@@ -3,12 +3,33 @@ using UnityEngine;
 public class test : MonoBehaviour {
     [Header("Attributes")]
     [SerializeField] private float speed = 5f ;
+    
+    [SerializeField] private string droite ;
+    [SerializeField] private string gauche ;
     [SerializeField] private float horizontal;
     [SerializeField] private Rigidbody2D rb;
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+
+
+        if (Input.GetKey(droite))
+        {
+           horizontal = 1f;
+        
+    
+    
+        }
+        else if (Input.GetKey(gauche))
+        {
+            horizontal = -1f;
+        }
+        
+
+        
+        else {horizontal = 0f;
+        }
+        
     }
 
     private void FixedUpdate() {
