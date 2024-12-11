061f969d8b95cbde77253ff40b5b9d17708c523a
florianBounon <florian.bounon@edu.esiee-it.fr>
2024-12-11 09:13:37 +0100

    jet

--- Assets/c charp/move.cs
+++ Assets/c charp/move.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
     [Header("Attributes")]
-    [SerializeField] private float speed = 5f ;
-    
+    [SerializeField] private float speed  ;
+    [SerializeField] private float jetforce  ;
+    [SerializeField] private float jet = 0f;
     [SerializeField] private string droite ;
     [SerializeField] private string gauche ;
-    [SerializeField] private float horizontal;
+    [SerializeField] private string jump ;
+    [SerializeField] private float horizontal = 0f;
     [SerializeField] private Rigidbody2D rb;
 
     void Update()
@@ -16,24 +18,27 @@ public class test : MonoBehaviour {
         if (Input.GetKey(droite))
         {
            horizontal = 1f;
-        
-    
-    
         }
         else if (Input.GetKey(gauche))
         {
             horizontal = -1f;
         }
-        
-
-        
         else {horizontal = 0f;
         }
-        
+
+
+
+
+        if (Input.GetKey(jump)){
+            jet = jetforce;
+        }
+        else{jet=0;}
     }
 
     private void FixedUpdate() {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        rb.AddForce(new Vector2(rb.linearVelocity.x, jet));
     }
 
 }
